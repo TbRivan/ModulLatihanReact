c9660c9c88317dd9df9073589c1ec556484b033a
tbrivan <tbrivan22@gmail.com>
2023-06-14 14:34:24 +0700

    finish

--- src/components/Fragments/TableData/index.tsx
+++ src/components/Fragments/TableData/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 interface TableDataProps {
@@ -13,6 +14,40 @@ interface TableProps {
 }
 
 export default function TableData({ tables, empty }: TableProps) {
+  const [sortKey, setSortKey] = useState("");
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+
+  // Function to handle table sorting
+  const handleSort = (key: string) => {
+    if (sortKey === key) {
+      // If the same key is clicked again, toggle the sort direction
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      // If a different key is clicked, set the sort key and default sort direction
+      setSortKey(key);
+      setSortDirection("asc");
+    }
+  };
+
+  // Function to sort the table data based on the sort key and direction
+  const sortTableData = (data: any[]) => {
+    const sortedData = [...data];
+    sortedData.sort((a, b) => {
+      const aValue = a[sortKey];
+      const bValue = b[sortKey];
+      if (aValue < bValue) {
+        return sortDirection === "asc" ? -1 : 1;
+      } else if (aValue > bValue) {
+        return sortDirection === "asc" ? 1 : -1;
+      }
+      return 0;
+    });
+    return sortedData;
+  };
+
+  // Sort the table data before rendering
+  const sortedTableData = sortTableData(tables);
+
   const handleDelete = (id: number) => {
     let dataLocal = JSON.parse(localStorage.getItem("data") || "[]");
     dataLocal = dataLocal.filter((item: any) => item.id !== id);
@@ -20,25 +55,29 @@ export default function TableData({ tables, empty }: TableProps) {
     window.location.href = "/";
   };
   return (
-    <table className=" text-center text-sm font-light mt-10">
-      <thead className="border-b bg-neutral-50 font-medium dark:border-neutral-500 dark:text-neutral-800">
+    <table className="w-1/2 text-center text-sm font-light mt-10 border-2">
+      <thead className="border-b bg-neutral-500 text-lg text-white">
         <tr>
-          <th scope="col" className=" px-6 py-4">
+          <th scope="col" className=" px-6 py-4 border">
             No
           </th>
-          <th scope="col" className=" px-6 py-4">
+          <th
+            onClick={() => handleSort("nama")}
+            scope="col"
+            className=" px-6 py-4 border hover:bg-neutral-400 cursor-ns-resize"
+          >
             Nama
           </th>
-          <th scope="col" className=" px-6 py-4">
+          <th scope="col" className=" px-6 py-4 border">
             Alamat
           </th>
-          <th scope="col" className=" px-6 py-4">
+          <th scope="col" className=" px-6 py-4 border">
             No Telp
           </th>
-          <th scope="col" className=" px-6 py-4">
+          <th scope="col" className=" px-6 py-4 border">
             Email
           </th>
-          <th scope="col" className=" px-6 py-4">
+          <th scope="col" className=" px-6 py-4 border">
             Action
           </th>
         </tr>
@@ -55,23 +94,23 @@ export default function TableData({ tables, empty }: TableProps) {
           </tr>
         )}
         {!empty &&
-          tables.length > 0 &&
-          tables.map((item, index) => {
+          sortedTableData.length > 0 &&
+          sortedTableData.map((item, index) => {
             return (
               <tr key={index} className="border-b dark:border-neutral-500">
-                <td className="whitespace-nowrap  px-6 py-4 text-center font-semibold text-lg">
+                <td className="whitespace-nowrap  px-6 py-4 text-center font-semibold text-lg border">
                   {index + 1}
                 </td>
-                <td className="whitespace-nowrap  px-6 py-4 font-semibold text-lg">
+                <td className="whitespace-nowrap  px-6 py-4 font-semibold text-lg border">
                   {item.nama}
                 </td>
-                <td className="whitespace-nowrap  px-6 py-4 font-semibold text-lg">
+                <td className="whitespace-nowrap  px-6 py-4 font-semibold text-lg border">
                   {item.alamat}
                 </td>
-                <td className="whitespace-nowrap  px-6 py-4 font-semibold text-lg">
+                <td className="whitespace-nowrap  px-6 py-4 font-semibold text-lg border">
                   {item.noTelp}
                 </td>
-                <td className="whitespace-nowrap  px-6 py-4 font-semibold text-lg">
+                <td className="whitespace-nowrap  px-6 py-4 font-semibold text-lg border">
                   {item.email}
                 </td>
                 <td className="w-[200px] whitespace-nowrap  px-6 py-4 flex justify-around">
